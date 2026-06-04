b8b653b78c3fcfdd862ab2ca78594ea85272fdd5
Nicholas Gorman <nickoboy1992@gmail.com>
2026-06-04 13:01:51 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.go
@@ -0,0 +1,47 @@
+import (
+    "cmp"
+    "slices"
+)
+
+func carFleet(target int, position []int, speed []int) int {
+    stacks := []car{}
+    for i, pos := range position {
+        c := car{
+            position: float64(pos),
+            speed: float64(speed[i]),
+        }
+        stacks = append(stacks, c)
+    }
+    //Now we sort the stacks of cars
+    slices.SortFunc(stacks, func(a, b car) int {
+        return cmp.Compare(a.position, b.position)
+    })
+
+    total := 0
+    for i:=len(stacks) - 1; i >=0; {
+        total ++
+        j:= i-1
+        //TIMETOARRIVE: pos + x(speed) = target
+        //This is rounded up - so (target-pos)/speed can be floor division
+        for j >= 0 && timeToTarget(stacks[i], target) >= timeToTarget(stacks[j], target) {
+            j--
+        }
+        i = j
+    }
+
+    return total
+}
+
+type car struct {
+    position float64
+    speed float64
+}
+
+func timeToTarget(c car, t int) float64 {
+    return (float64(t) - c.position)/c.speed
+}
+
+//Monotonicity - SORTING the position array, but we need to have it associated speeds stay sorted with it
+//THEN - time to target
+
+//My solutions space complexity does not feel like it will be optimal
